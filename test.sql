542c80bf962fd32092e4f35c316a328c22565fc5
Samary Cardozo <159805400+codekirari@users.noreply.github.com>
2025-02-11 11:51:16 -0500

    Update test.sql
    password

--- test.sql
+++ test.sql
@@ -1,6 +1,51 @@
 -- Usar la base de datos existente
 USE RegistroLoginDB;
 GO
+-- Eliminar la tabla si ya existe
+DROP TABLE IF EXISTS Usuarios;
+
+
+-- Eliminar los procedimientos almacenados si ya existen
+DROP PROCEDURE IF EXISTS sp_RegistrarUsuario;
+GO
+
+DROP PROCEDURE IF EXISTS sp_LoginUsuario;
+GO
+
+
+CREATE TABLE Usuarios (
+    UsuarioID INT IDENTITY(1,1) PRIMARY KEY, 
+    Nombre NVARCHAR(100),                    
+    Apellido NVARCHAR(100),                 
+    NombreUsuario NVARCHAR(50) UNIQUE,        
+    Password NVARCHAR(255)              
+);
+GO
+
+
+CREATE PROCEDURE sp_RegistrarUsuario
+    @Nombre NVARCHAR(100),
+    @Apellido NVARCHAR(100),
+    @NombreUsuario NVARCHAR(50),
+    @Password NVARCHAR(255)
+AS
+BEGIN
+    INSERT INTO Usuarios (Nombre, Apellido, NombreUsuario, Password)
+    VALUES (@Nombre, @Apellido, @NombreUsuario, @Password);
+END;
+GO
+
+-- Crear un procedimiento almacenado para verificar un usuario en el login
+CREATE PROCEDURE sp_LoginUsuario
+    @NombreUsuario NVARCHAR(50),
+    @Contraseña NVARCHAR(255)
+AS
+BEGIN
+    SELECT * FROM Usuarios
+    WHERE NombreUsuario = @NombreUsuario
+    AND Password = @Contraseña;
+END;
+GO
 
 -- Eliminar la tabla si ya existe
 DROP TABLE IF EXISTS Usuarios;
